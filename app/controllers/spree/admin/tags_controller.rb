f6a4ba3b6ac5805a10d1482957e2cbac42d14fae
Yong Zhao <oracle.zhao2008@gmail.com>
2022-03-14 11:33:30 -0400

    show products in tag show

--- app/controllers/spree/admin/tags_controller.rb
+++ app/controllers/spree/admin/tags_controller.rb
@@ -1,10 +1,21 @@
 module Spree
   module Admin
     class TagsController < ResourceController
+      helper Spree::ProductsHelper
       respond_to :html
 
       def index
         @tags = Spree::Tag.order(:id)
+      end
+
+      def show
+        @products = @tag.products.includes([
+            { 
+              variants: [:images], 
+              master: [:images, :default_price] 
+            }
+          ]).page(params[:page]).per(params[:per_page] || 50)
+        
       end
 
     end
